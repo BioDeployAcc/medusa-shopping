1a9bd097d5a18ef71a9451ea82b6ce820fe8df54
BioDeployAcc <biosfera.trema@gmail.com>
2024-06-24 20:44:40 +0200

    added filter component logic and styles

--- frontend/src/components/filter/Filter.tsx
+++ frontend/src/components/filter/Filter.tsx
@@ -1,6 +1,11 @@
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
+import classes from "./Filter.module.scss";
 import { z } from "zod";
+import Input from "../input";
+import SelectInput from "../selectInput";
+import { useCollections, useProductCategories } from "medusa-react";
+import Link from "next/link";
 
 export const filterSchema = z.object({
   q: z.string().optional(),
@@ -8,7 +13,8 @@ export const filterSchema = z.object({
   category_id: z.string().optional(),
   gte: z.number().optional(),
   lte: z.number().optional(),
-  page: z.number().optional(),
+  order: z.string().optional(),
+  direction: z.string().optional(),
   limit: z.number().optional(),
 });
 
@@ -20,8 +26,6 @@ export const defaultQuery: QueryType = {
   category_id: "",
   gte: 0,
   lte: 1000,
-  page: 1,
-  limit: 10,
 };
 
 export interface FilterProps {
@@ -33,4 +37,91 @@ export const Filter = ({ defaultValues = defaultQuery }) => {
     resolver: zodResolver(filterSchema),
     defaultValues,
   });
+
+  const { product_categories } = useProductCategories();
+  const { collections } = useCollections();
+
+  return (
+    <form className={classes.container}>
+      <Input form={form} label="Search" attribute="q" />
+      <Input form={form} label="Min Price" attribute="gte" isNumeric />
+      <Input form={form} label="Max Price" attribute="lte" isNumeric />
+      <SelectInput
+        form={form}
+        label="Category"
+        attribute="category_id"
+        options={[
+          {
+            label: "All",
+            value: "",
+          },
+          ...(product_categories?.map((c) => ({
+            label: c.name,
+            value: c.id,
+          })) || []),
+        ]}
+      />
+      <SelectInput
+        form={form}
+        label="Collection"
+        attribute="collection_id"
+        options={[
+          {
+            label: "All",
+            value: "",
+          },
+          ...(collections?.map((c) => ({
+            label: c.title,
+            value: c.id,
+          })) || []),
+        ]}
+      />
+      <SelectInput
+        form={form}
+        label="Order"
+        attribute="order"
+        options={[
+          { label: "Name", value: "title" },
+          { label: "Category", value: "category_id" },
+          {
+            label: "Collection",
+            value: "collection_id",
+          },
+          { label: "Price", value: "variants.prices.amount" }, //See how to reference the price field
+        ]}
+      />
+      <SelectInput
+        form={form}
+        label="Direction"
+        attribute="direction"
+        options={[
+          { label: "Ascending", value: "" },
+          { label: "Descending", value: "-" },
+        ]}
+      />
+      <SelectInput
+        form={form}
+        label="Limit"
+        attribute="limit"
+        options={[
+          { label: "12", value: "12" },
+          { label: "24", value: "24" },
+          { label: "48", value: "48" },
+        ]}
+      />
+      <Link
+        type="submit"
+        className={classes.button}
+        href={{
+          pathname: "/search",
+          query: {
+            ...form.getValues(),
+            order: `${form.watch("direction")}${form.watch("order")}`, //check is mapping ok here
+          },
+        }}
+      >
+        Search
+      </Link>
+    </form>
+  );
 };
